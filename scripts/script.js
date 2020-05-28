52146f6de5bdec7c8a3aa99c5bf176c0d8027f29
Gustavo <gstonin95@gmail.com>
2020-05-28 08:56:22 +0100

    working big numbers, big decimals, changed all logic to inputValues, all Buttons working as intended

--- scripts/script.js
+++ scripts/script.js
@@ -25,35 +25,55 @@ function sqrt(a) {
 	return Math.sqrt(a);
 }
 
-function operate (arr) {                                    //[number, operator(string), number]
+function numDigits(num) {                                           //math function to count number of digits before decimals     
+
+    return Math.max(Math.floor(Math.log10(Math.abs(num))), 0) + 1;
+}
+
+function operate (arr) {                                                             //[number, operator(string), number]
+
+    let result;
 
     switch (arr[1]) {
         
         case '+':
-            return add(arr[0],arr[2]);
+            result = add(arr[0],arr[2]);
         break;
 
         case '-':
-            return subtract(arr[0],arr[2]);
+            result = subtract(arr[0],arr[2]);
         break;
 
         case '/':
-            return divide(arr[0],arr[2]);
+            result = divide(arr[0],arr[2]);
         break;
 
         case '*':
-            return multiply(arr[0],arr[2]);
+            result = multiply(arr[0],arr[2]);
         break;
 
         case 's':
-            return sqrt(arr[0]);
-        break;
-
-        case 'm':
-            return arr[0]*(-1);
+            result = sqrt(arr[0]);
         break;
     }
 
+    if (result === 'Err0r') 
+    {
+        return result; 
+    
+    } else {    
+        
+        if (numDigits(result) > 12) 
+        {
+            return result; 
+
+        } else {
+        
+            for (let i = 0; i <= 11; i++) {                                                   //rounds to match 12 digits on display
+                if (numDigits(result) == (12-i)) return +result.toFixed(i); 
+            }
+        }
+    }
 }
 
 const containerCalculator = document.querySelector('.containerCalculator');
@@ -75,7 +95,7 @@ let pressedButton;
 let inputValues = [];
 let checkDisplayClear;
 let currentValue;
-let temp;
+let replacedNumber;
 
 function resetQueue() {
     inputValues = [];
@@ -85,6 +105,11 @@ function clearDisplay() {
     display.textContent = '0';
     checkDisplayClear = false;
 }                        
+
+function isThereADot(str) {                                                             //checks for a dot on a string
+
+    return str.indexOf('.') < 0 ? false : true;
+}
 
 const buttons = document.querySelectorAll('.singleButton');
 buttons.forEach(button => button.addEventListener('click', (e) => {
@@ -98,12 +123,23 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
 
         if (display.textContent === '0')  display.textContent = '';                     //overwriting zero from clear display
 
-        display.textContent += pressedButton[3];     
-        
-            if (inputValues.length == 0) inputValues[0] = Number(display.textContent);
+        if (Array.from(display.textContent).length > 11) {                              //if its bigger than 12 digits, starts substituting number entered
+            replacedNumber = Array.from(display.textContent);
+            replacedNumber.shift();
+            replacedNumber.push(pressedButton[3]);
 
-            if (inputValues.length == 2) inputValues[2] = Number(display.textContent);
-        
+            if (replacedNumber[0] === '.') replacedNumber.shift();                      //removes the dot if its alone before the number
+
+            display.textContent = replacedNumber.join('');
+
+        } else {
+
+            display.textContent += pressedButton[3];     
+        }
+            if (inputValues.length == 0 || inputValues.length == 1) inputValues[0] = Number(display.textContent);
+
+            if (inputValues.length == 2 || inputValues.length == 3) inputValues[2] = Number(display.textContent);
+           
         clearButton.textContent = 'C';                                   
     }
 
@@ -113,13 +149,20 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
         case '+':
         case '-':
 
-            if (inputValues.length == 3) {                                                  //checks if there is already a number and an operation in queue        
+            if (inputValues.length == 3) {                                                              //checks if there is already a number and an operation in queue        
                 
                     currentValue = operate(inputValues);
-                    display.textContent = currentValue;
+
+                    if (numDigits(currentValue) > 12)                                                   //transforms big numbers into exponential form, so it doenst expand the display
+                    {
+                        display.textContent = currentValue.toExponential(2);
+                    } else {
+                        display.textContent = currentValue;
+                    }
+                    
                     clearButton.textContent = 'CE';
 
-                    if (currentValue === 'Err0r') {                                         //checks if math function returns Err0r
+                    if (currentValue === 'Err0r') {                                                     //checks if math function returns Err0r
                         currentValue = 0;
                         resetQueue();
                     
@@ -154,7 +197,14 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
             if (inputValues.length == 3) {                                  
 
                 currentValue = operate(inputValues);
-                display.textContent = currentValue;
+
+                if (numDigits(currentValue) > 12)                                                   //transforms big numbers into exponential form, so it doenst expand the display
+                    {
+                        display.textContent = currentValue.toExponential(2);
+                    } else {
+                        display.textContent = currentValue;
+                    }
+
                 clearButton.textContent = 'CE';
 
                 if (currentValue === 'Err0r') {                                         //checks if math function returns Err0r
@@ -195,7 +245,14 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
                 if (inputValues.length == 1) inputValues.push(pressedButton[0]);
 
                 currentValue = operate(inputValues);
-                display.textContent = currentValue;
+                
+                if (numDigits(currentValue) > 12)                                                   //transforms big numbers into exponential form, so it doenst expand the display
+                    {
+                        display.textContent = currentValue.toExponential(2);
+                    } else {
+                        display.textContent = currentValue;
+                    }
+                   
                 clearButton.textContent = 'CE';
 
                     if (currentValue === 'Err0r') {                                     //checks if math function returns Err0r
@@ -216,16 +273,15 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
 
         case 'm':
         
-            if (inputValues.length == 3) {                                              //changes the signal of last number on queue
+            let index;
 
-                inputValues[2] = inputValues[2]*(-1);
-                display.textContent = inputValues[2];
-            }
-            if (inputValues.length == 2 || inputValues.length == 1) {
+            if (inputValues.length == 3) index = 2;                            
 
-                inputValues[0] = inputValues[0]*(-1);
-                display.textContent = inputValues[0];
-            }
+            if (inputValues.length == 2 || inputValues.length == 1) index = 0; 
+
+            inputValues[index] = inputValues[index]*(-1);                                             //changes the signal of last number on queue
+                
+            numDigits(inputValues[index]) > 12 ? display.textContent = inputValues[index].toExponential(2) : display.textContent = inputValues[index];         //transforms big numbers into exponential form, so it doenst leak in the display
 
         break;
         
@@ -233,16 +289,12 @@ buttons.forEach(button => button.addEventListener('click', (e) => {
 
             if (inputValues.length > 0) {
 
-                if (display.textContent.indexOf('.') < 0)  {                            //checks for dot on display (last number queued)
-
-                    display.textContent += '.';
-                    resetQueue();  
-                }             
-            }
+                if (!isThereADot(display.textContent))  display.textContent += '.';
+            }            
 
             if (inputValues.length == 0) {
 
-                if (display.textContent.indexOf('.') < 0)    display.textContent = '0.';
+                if (!isThereADot(display.textContent))  display.textContent = '0.';
             }    
 
         break;
